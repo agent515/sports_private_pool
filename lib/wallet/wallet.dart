fdd62e607957cb66969babbe54fb9ac44a98214c
aga7hokakological <iamsaurabhmandavkar@gmail.com>
2021-04-20 17:59:17 +0530

    payment gateway checkout

--- lib/wallet/wallet.dart
+++ lib/wallet/wallet.dart
@@ -1,7 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:sports_private_pool/api.dart';
-import 'package:razorpay_flutter/razorpay_flutter.dart';
+import 'package:sports_private_pool/wallet/Payment_Screen.dart';
 
 class MyWallet extends StatefulWidget {
   @override
@@ -10,82 +9,40 @@ class MyWallet extends StatefulWidget {
 
 class _MyWalletState extends State<MyWallet> {
 
-  Razorpay razorpay;
-  TextEditingController textEditingController = new TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-
-    razorpay = new Razorpay();
-
-    razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlerPaymentSuccess);
-    razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlerPaymentFailure);
-    razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handlerExternalWallet);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    razorpay.clear();
-  }
-
-  void openCheckOut() {
-    var options = {
-      'key': '$razorpayApi',
-      'amount': textEditingController.text,
-      'name': 'Envision',
-      'description': 'Payment',
-      'prefill': {
-        'contact': '9876543210',
-        'email': 'joey@gmail.com',
-      },
-      'external': {
-        'wallets': ['paytm']
-      }
-    };
-
-    try {
-      razorpay.open(options);
-    } catch(e) {
-      print(e.toString());
-    }
-
-  }
-
-  void handlerPaymentSuccess() {
-    print("Payment Success");
-  }
-
-  void handlerPaymentFailure() {
-    print("Payment Error");
-  }
-
-  void handlerExternalWallet() {
-    print("External Wallet");
-  }
+  TextEditingController _amountController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: Column(
-        children: [
-          TextField(
-            controller: textEditingController,
-            decoration: InputDecoration(
-              hintText: "Enter Amount To add",
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          children: [
+            // Text(
+            //   '₹ ' + loggedInUserData['purse'].toString(),
+            //   style: TextStyle(
+            //     color: Colors.white,
+            //     fontWeight: FontWeight.bold,
+            //   ),
+            // ),
+            // SizedBox(height: 12,),
+            TextField(
+              controller: _amountController,
+              decoration: InputDecoration(
+                hintText: "Enter Amount To add",
+              ),
             ),
-          ),
-          SizedBox(height: 12,),
-          ElevatedButton(
-            child: Text('Add Now'),
-            onPressed: () {
-              openCheckOut();
-            },
-          )
-        ],
-      ),
+            SizedBox(height: 12,),
+            ElevatedButton(
+              child: Text('Add Now'),
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => PaymentScreen(amount: _amountController.text,)));
+              },
+            )
+          ],
+        ),
+      )
     ),
     );
   }
